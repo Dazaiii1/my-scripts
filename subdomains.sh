a5c99e0ba6adeddfa4402e5a601bd2e6a4a9ef91
Dazaii__ <69427610+Abdennour-1@users.noreply.github.com>
2022-02-04 13:14:43 +0100

    Create subdomains.sh

---
+++ subdomains.sh
@@ -0,0 +1,12 @@
+if [ $1 = '-h' ]
+then 
+	echo "Example: "
+	echo "./subdomains.sh file.txt"
+
+else
+  mkdir results
+  cat $1 | while read domain do ; do amass enum -passive -norecursive -noalts -d $domain -o $domain.amass ;done
+  cat $1 | while read domain do ; do subfinder -d $domain -silent -t 100 | tee $domain.subfinder ;done
+  cat domains.txt | while read domain do ; do cat $domain.* | tee results/$domain.txt ;done
+  rm *.subfinder ; rm *.amass
+fi
