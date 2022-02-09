2d8a2ee1c97dc5c0f7216dc76188c44f0df5a328
Dazaii__ <69427610+Abdennour-1@users.noreply.github.com>
2022-02-09 15:18:50 +0100

    Update tld-enum.sh

--- tld-enum.sh
+++ tld-enum.sh
@@ -7,10 +7,10 @@ then
   echo "company2.*"
   echo "Note 2 : tlds.txt" 
   echo "download it from https://gist.githubusercontent.com/wridgers/1968862/raw/ca71d75354fc2fc833a62a00f15f5dc0180b2ee9/tlds.txt"
-  echo "and then remove points from the wordlist : sed -i 's/\.//' tlds.txt "
 
 else
 
+sed -i 's/\.//' $2
 sed 's/\.\*//' $1  |  while read url do; do ffuf -u https://$url.FUZZ/ -w $2 -s | while read tld do; do echo $url.$tld | tee -a $3 ;done ;done
 
 fi
