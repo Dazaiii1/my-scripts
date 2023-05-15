15e9a8ddc3fbfcf9a3d6a8126a5e62ba8a2616f0
Dazaii__ <69427610+Dazaiii1@users.noreply.github.com>
2023-05-16 00:04:02 +0100

    Update rustscan.sh

--- rustscan.sh
+++ rustscan.sh
@@ -1,1 +1,7 @@
+#2022 Way
+
 cat $1 | while read domain ;do rustscan -a $domain --ulimit 5000 | grep Open | awk '{print $2}' | sed "s/^.*://;s/^/$domain:/" ;done
+
+#2023 way ( Json Format )
+
+echo ncs.nintendo.com | while read domain; do rustscan -a $domain --ulimit 5000 | grep Open | while read line; do ip=$(echo "$line" | cut -d' ' -f2); port=${line##*:}; echo "{\"ip\": \"$ip\", \"domain\": \"$domain:$port\"}"; done; done
